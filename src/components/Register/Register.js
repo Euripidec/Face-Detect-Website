9e2fc79a44157110dfd1feddabb8d362dcd8deec
Euripidec <111103974+Euripidec@users.noreply.github.com>
2023-05-14 22:04:45 -0400

    Update Register.js

--- src/components/Register/Register.js
+++ src/components/Register/Register.js
@@ -23,7 +23,7 @@ class Register extends React.Component {
   }
 
   onSubmitSignIn = () => {
-    fetch('http://localhost:3000/register', {
+    fetch('https://backend-2xrn.onrender.com/register', {
       method: 'post',
       headers: {'Content-Type': 'application/json'},
       body: JSON.stringify({
