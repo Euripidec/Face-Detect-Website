b7827600b73b82e2b1b0dca13644c3122b788641
Euripidec <111103974+Euripidec@users.noreply.github.com>
2023-05-14 22:03:34 -0400

    Update App.js

--- src/App.js
+++ src/App.js
@@ -110,7 +110,7 @@ class App extends Component {
       .then((response) => response.json())
       .then(response => {
         if (response) {
-          fetch('http://localhost:3000/image', {
+          fetch('https://backend-2xrn.onrender.com/image', {
             method: 'put',
             headers: {'Content-Type': 'application/json'},
             body: JSON.stringify({
